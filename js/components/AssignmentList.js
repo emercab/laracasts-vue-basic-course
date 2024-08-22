2794b06379f7f8662f3d651f0b95009c98b9e538
Emerson Cabrera <info@emersoncabrera.com>
2024-08-21 19:45:31 -0500

    Tags added.

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,23 +1,32 @@
 import AssignmentItem from "./AssignmentItem.js"
+import AssignmentTags from "./AssignmentTags.js"
 
 export default {
   components: {
     AssignmentItem,
+    AssignmentTags,
   },
 
   template: /*html*/ `
     <section v-show="assignments.length" class="mb-8">
 
-      <h2 class="font-bold text-2xl mb-2 text-center">{{ title }}</h2>
+      <h2 class="font-bold text-2xl mb-2 text-center">
+        {{ title }}
+        <span class="text-gray-500">({{ assignments.length }})</span>
+      </h2>
 
-      <ul class="border border-gray-600 rounded-md divide-y divide-gray-600 max-w-60 mx-auto">
-        
+      <assignment-tags
+        :tags="assignments.map(a => a.tag)"
+        :currentTag
+        @changeTag="currentTag = $event"
+      />
+
+      <ul class="border border-gray-600 rounded-md divide-y divide-gray-600 max-w-60 mt-4 mx-auto">
         <assignment-item
-          v-for="assignment in assignments"
+          v-for="assignment in filteredAssignments"
           :key="assignment.id"
           :assignment="assignment"
         />
-
       </ul>
 
     </section>
@@ -27,4 +36,20 @@ export default {
     assignments: Array,
     title: String,
   },
+
+  data() {
+    return {
+      currentTag: 'all',
+    }
+  },
+
+  computed: {
+    filteredAssignments() {
+      if (this.currentTag === 'all') {
+        return this.assignments
+      }
+      return this.assignments.filter( assignment => assignment.tag === this.currentTag )
+    },
+  },
+
 }
---
+++ js/components/AssignmentTags.js
@@ -0,0 +1,28 @@
+export default {
+  template: /*html*/ `
+    <div class="flex gap-2 justify-center">
+      <button
+        @click="$emit('changeTag', tag)"
+        v-for="tag in allTags"
+        class="border rounded px-1 py-px text-xs hover:bg-blue-500 hover:border-blue-600 duration-300"
+        :class="{
+          'bg-blue-500 border-blue-600' : currentTag === tag,
+          'bg-gray-600 border-gray-500' : currentTag !== tag,
+        }"
+      >{{ tag }}
+    </button>
+  </div>
+  `,
+
+  props: {
+    tags: Array,
+    currentTag: String,
+  },
+
+  computed: {
+    allTags() {
+      // Return a new Set of tags from the assignments
+      return ['all', ... new Set(this.tags) ];
+    }
+  }
+}
